a7ce9e36fd833dbc5fc30675351fba1c6341632a
A S <65203960+asathsara@users.noreply.github.com>
2025-08-21 16:25:40 +0530

    implement csrf token way

--- src/main/java/com/example/springsecuritybasics/HelloController.java
+++ src/main/java/com/example/springsecuritybasics/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
-    public String greet(){
+    public String greet() {
         return "Hello World";
     }
 }
