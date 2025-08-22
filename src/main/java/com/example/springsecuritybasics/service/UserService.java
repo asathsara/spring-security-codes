e127da72cb83484dc91554d783a082da989f8ced
A S <65203960+asathsara@users.noreply.github.com>
2025-08-22 20:10:40 +0530

    add bcrypt encoder to hash user passwords

--- src/main/java/com/example/springsecuritybasics/service/UserService.java
+++ src/main/java/com/example/springsecuritybasics/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.springsecuritybasics.service;
 import com.example.springsecuritybasics.model.Users;
 import com.example.springsecuritybasics.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,13 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
     public Users register(Users user){
+
+        // Bcrypt the password
+        user.setPassword(encoder.encode(user.getPassword()));
+
         userRepository.save(user);
         return user;
     }
